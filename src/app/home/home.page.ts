f7c0c5670b573e35d8facc1e8ce5d8f08f532a2a
pittalaranjeira <vitoria.pitta01@fatec.sp.gov.br>
2023-02-21 21:53:53 -0300

    simulador ibge - renda básica com relatorio em popover

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+
+  mediarenfa = 0;
+  numpessoas = 0;
+  resproprias = 0;
+  resalugadas = 0;
+  residencias = 0;
+  mediafinal = 0;
+
+  coldados(formu: { value: { renfa: any; numpe: any; tpres: any; }; }) {
+    const renfa = formu.value.renfa;
+    const numpe = formu.value.numpe;
+    const tpres = formu.value.tpres;
+
+    this.mediarenfa += parseFloat(renfa);
+    this.numpessoas += parseFloat(numpe);
+
+    if(tpres == "P"){
+      this.resproprias++;
+      this.residencias++;
+    } else if(tpres == "A"){
+      this.resalugadas++;
+      this.residencias++;
+    }
+
+    this.mediafinal = this.mediarenfa/this.residencias;
+    
+  }
 
 }
